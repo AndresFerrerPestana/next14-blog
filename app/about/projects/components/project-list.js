ef8138b47bc0b69900323f3222f67f2f5502691d
Andrés Ferrer <andres.ferrer.dev@gmail.com>
2024-05-19 19:33:36 +0100

    grid reusability

--- app/about/projects/components/project-list.js
+++ app/about/projects/components/project-list.js
@@ -1,3 +1,5 @@
+import Card from "@/components/card"
+
 export default async function ProjectList() {
   const response = await fetch(
     'http://localhost:3001/repos',
@@ -6,12 +8,16 @@ export default async function ProjectList() {
   const repos = await response.json()
 
   return (
-    <ul>
+    <ul className="grid grid-cols-1 md:grid-cols-2 gap-4">
       {repos.map(repo => (
         <li key={repo.id} className="mb-4">
-          <div>{repo.title}</div>
+          <Card className="font-mono h-full">
+            <div className="flex justify-between items-center mb-4">
+              <div className="font-semibold">{repo.title}</div>
+              <div>⭐{repo.stargazers_count}</div>
+            </div>
           <div>{repo.description}</div>
-          <div>{repo.stargazers_count}</div>
+          </Card>
         </li>
       ))}
     </ul>
