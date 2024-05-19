990906240a525ae0f3ec9c167a968f9876520d6e
Andrés Ferrer <andres.ferrer.dev@gmail.com>
2024-05-19 15:50:11 +0100

    using suspense

--- app/about/projects/page.js
+++ app/about/projects/page.js
@@ -1,6 +1,10 @@
 // Opt out of caching for all data requests in the route segment
 // export const dynamic = 'force-dynamic'
 
+import { Suspense } from "react";
+import ProjectList from "./components/project-list";
+import ProjectListLoading from "./components/project-list-loading";
+
 export default async function ProjectsPage () {
     const response = await fetch('http://localhost:3001/repos', { cache: 'no-store' });
     const repos = await response.json(); 
@@ -8,16 +12,12 @@ export default async function ProjectsPage () {
     return(
     <div className="p-20">
         <h1 className="mb-8 text-xl">Projects</h1>
+
+        <div className="mb-8">Hello, this is the list of my repos!</div>
         
-        <ul>
-            { repos.map(repo => (
-             <li key={repo.id} className="mb-4">
-                <div>{repo.title}</div>
-                <div>{repo.description}</div>
-                <div>{repo.stargazers_count}</div>
-             </li>   
-            )) }
-        </ul>
+        <Suspense fallback={<ProjectListLoading />}>
+            <ProjectList />
+        </Suspense>
     </div>
 );
 }
