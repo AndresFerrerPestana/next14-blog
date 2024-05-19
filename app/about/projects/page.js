499ccad1e34d576927d2508ebf8470906da69199
Andrés Ferrer <andres.ferrer.dev@gmail.com>
2024-05-19 21:11:59 +0100

    react ErrorBoundary

--- app/about/projects/page.js
+++ app/about/projects/page.js
@@ -1,21 +1,19 @@
-// Opt out of caching for all data requests in the route segment
-// export const dynamic = 'force-dynamic'
-
 import { Suspense } from "react";
 import ProjectList from "./components/project-list";
 import ProjectListLoading from "./components/project-list-loading";
+import { ErrorBoundary } from "react-error-boundary";
 
 export default async function ProjectsPage () {
     
     return(
-    <div>
-        <h1 className="mb-8 text-xl">Projects</h1>
-
-        <div className="mb-8">Hello, this is the list of my repos!</div>
-        
-        <Suspense fallback={<ProjectListLoading />}>
-            <ProjectList />
-        </Suspense>
-    </div>
-);
+        <div>
+            <h1 className="mb-8 text-xl">Projects</h1>
+            <div className="mb-8">Hello, this is the list of my repos!</div>           
+            <ErrorBoundary fallback={<div>Cannot fetch projects currently</div>}>
+                <Suspense fallback={<ProjectListLoading />}>
+                    <ProjectList />
+                </Suspense>
+            </ErrorBoundary>           
+        </div>
+    )
 }
