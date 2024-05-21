30b8c53f2d3ef5bf0b5e5b346ed5013dd8e3f359
Andrés Ferrer <andres.ferrer.dev@gmail.com>
2024-05-21 05:29:20 +0100

    recovering from errors

--- app/about/error.js
+++ app/about/error.js
@@ -1,12 +1,16 @@
 'use client' // Error components must be Client Components
  
 import { useEffect } from 'react'
+import {useRouter} from 'next/navigation'
+import {startTransition} from 'react'
  
 export default function Error({ error, reset }) {
   useEffect(() => {
     // Log the error to an error reporting service
     console.error(error)
   }, [error])
+
+  const router = useRouter()
  
   return (
     <div>
@@ -14,7 +18,12 @@ export default function Error({ error, reset }) {
       <button
         onClick={
           // Attempt to recover by trying to re-render the segment
-          () => reset()
+          () => {
+            startTransition(() => {
+              router.refresh()
+              reset()
+            })
+          }
         }
       >
         Try again
