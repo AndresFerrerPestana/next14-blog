8c8da5a58139a9788a0bf67a01a27748298256fd
Andrés Ferrer <andres.ferrer.dev@gmail.com>
2024-05-21 16:32:17 +0100

    using image component

--- app/photos/page.js
+++ app/photos/page.js
@@ -1,3 +1,10 @@
+import Image from 'next/image'
+
+import dog1 from './../../public/images/dog1.png'
+import dog2 from './../../public/images/dog2.png'
+import dog3 from './../../public/images/dog3.png'
+import dog4 from './../../public/images/dog4.png'
+
 export default function PhotosPage () {
     return (
         <div>
@@ -5,18 +12,34 @@ export default function PhotosPage () {
 
             <div className="grid grid-cols-2 gap-4">
                 <div className="h-60">
-                    <img src="/images/dog1.png" className="object-cover w-full h-full" />
+                    {/* <img src="/images/dog1.png" className="object-cover w-full h-full" /> */}                   
+                    <Image 
+                        src={dog1} 
+                        alt="Picture from my Dog1"
+                        className="object-cover w-full h-full"
+                    />
+                </div>
+                <div className="h-60">               
+                    <Image 
+                        src={dog2} 
+                        alt="Picture from my Dog2"
+                        className="object-cover w-full h-full"
+                    />
+                </div>
+                <div className="h-60">                  
+                    <Image 
+                        src={dog3} 
+                        alt="Picture from my Dog3"
+                        className="object-cover w-full h-full"
+                    />
                 </div>
                 <div className="h-60">
-                    <img src="/images/dog2.png" className="object-cover w-full h-full" />
-                </div>
-                <div className="h-60">
-                    <img src="/images/dog3.png" className="object-cover w-full h-full" />
-                </div>
-                <div className="h-60">
-                    <img src="/images/dog4.png" className="object-cover w-full h-full" />
-                </div>
-                
+                    <Image 
+                        src={dog4} 
+                        alt="Picture from my Dog4"
+                        className="object-cover w-full h-full"
+                    />
+                </div>       
             </div>
         </div>
 
